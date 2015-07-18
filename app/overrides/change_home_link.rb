507ecb92dba2333442f7d86d69797aae73e91582
Ian Manseau <manseauian@gmail.com>
2015-07-18 14:03:27 +0000

    Replaced main nav bar template

--- app/overrides/change_home_link.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
-  :name => 'change home link',
-  :replace => 'li',
-  :attributes => {:id => "home-link"},
-  :text => '
-    <li id="index-link" data-hook><%= link_to "Home", "/" %></li>
-    <li id="home-link" data-hook><%= link_to Spree.t(:shop), spree.root_path %></li>
-')
